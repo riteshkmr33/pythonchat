23cd9913c9ea43787e3189d3df085574cf29abf9
Hemant Kumar <hemant@clavax-62>
2016-04-21 17:44:57 +0530

    updated gui

--- gui/pychat.py
+++ gui/pychat.py
@@ -6,75 +6,71 @@
 #import sys
  
 #def prompt():
- #   sys.stdout.write('<> ')
-  #  sys.stdout.flush()
+    #   sys.stdout.write('<> ')
+        #  sys.stdout.flush()
  
 #main function
 #if __name__ == "__main__":
     
- #   user = 'Guest'
-  #  host = 'localhost'
-   # port = 6000
+    #   user = 'Guest'
+        #  host = 'localhost'
+            # port = 6000
      
-    #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #s.settimeout(2)
+                #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                #s.settimeout(2)
      
-    # connect to remote host
-   # try:
-    #    s.connect((host, port))
-    #except:
-     #   print 'Unable to connect'
-      #  sys.exit()
+                # connect to remote host
+            # try:
+                #    s.connect((host, port))
+                #except:
+                    #   print 'Unable to connect'
+                        #  sys.exit()
      
-    #print 'Connected to chat. Start sending messages'
+                #print 'Connected to chat. Start sending messages'
     
-    #if user == 'Guest':
-     #   sys.stdout.write('<' + user + '> Please enter your name to start chat :')
-     #   user = sys.stdin.readline().rstrip('\n')
-       # s.send(json.dumps({'username': user}))
-      #  sys.stdout.write('\n<you>:')
+                #if user == 'Guest':
+                    #   sys.stdout.write('<' + user + '> Please enter your name to start chat :')
+                    #   user = sys.stdin.readline().rstrip('\n')
+                        # s.send(json.dumps({'username': user}))
+                    #  sys.stdout.write('\n<you>:')
      
-    #while 1:
-     #   socket_list = [sys.stdin, s]
+                #while 1:
+                    #   socket_list = [sys.stdin, s]
 
-        # Get the list sockets which are readable
-      #  read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
+                        # Get the list sockets which are readable
+                    #  read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
 
-      #  for sock in read_sockets:
-            #incoming message from remote server
-      #      if sock == s:
-       #         data = json.loads(sock.recv(4096))
-       #         if not data:
-       #             print '\nDisconnected from chat server'
-       #            sys.exit()
-        #        else:
-        #            #print data
-        #            sys.stdout.write('\n<' + data['username'] + '>: ' + data['msg'])
-        #            sys.stdout.write('\n<you>:')
-         #           sys.stdout.flush()
+                        #  for sock in read_sockets:
+                            #incoming message from remote server
+                        #      if sock == s:
+                            #         data = json.loads(sock.recv(4096))
+                            #         if not data:
+                            #             print '\nDisconnected from chat server'
+                            #            sys.exit()
+                                #        else:
+                                #            #print data
+                                #            sys.stdout.write('\n<' + data['username'] + '>: ' + data['msg'])
+                                #            sys.stdout.write('\n<you>:')
+                                    #           sys.stdout.flush()
 
-            #user entered a message
-         #   else:
-         #       msg = sys.stdin.readline()
-          #      s.send(json.dumps({'username': user, 'msg': msg.rstrip('\n')}))
-          #      sys.stdout.write('\n<you>:')
-          #      sys.stdout.flush()
-            
-#!/usr/bin/python
-# -*- coding: iso-8859-1 -*-
+                                        #user entered a message
+                                    #   else:
+                                    #       msg = sys.stdin.readline()
+                                        #      s.send(json.dumps({'username': user, 'msg': msg.rstrip('\n')}))
+                                        #      sys.stdout.write('\n<you>:')
+                                        #      sys.stdout.flush()
 
 import Tkinter
-import json
-import select
-import socket
-import string
-import sys
 import threading
+import socket
+import random
+import math
+import json
 
 class simpleapp_tk(Tkinter.Tk):
     
     user = 'Guest'
-    host = '127.0.0.1'
+    host = '192.168.2.132'
     port = 6000
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
@@ -90,35 +86,54 @@ class simpleapp_tk(Tkinter.Tk):
         # connect to remote host
         try:
             self.sock.connect((self.host, self.port))
-            status = 'Connected to chat. Please enter your name'
+            status = 'Connected to chat. Please enter your name \n'
         except:
-            status = 'Unable to connect'
+            status = 'Unable to connect \n'
             #sys.exit()
+            
+        self.mainBody = Tkinter.Frame(self, height=20, width=50)
 
-        self.grid()
-
+        self.mainBodyText = Tkinter.Text(self.mainBody)
+        self.mainBodyTextScroll = Tkinter.Scrollbar(self.mainBody)
+        self.mainBodyText.focus_set()
+        self.mainBodyTextScroll.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
+        self.mainBodyText.pack(side=Tkinter.LEFT, fill=Tkinter.Y)
+        self.mainBodyTextScroll.config(command=self.mainBodyText.yview)
+        self.mainBodyText.config(yscrollcommand=self.mainBodyTextScroll.set)
+        self.mainBody.pack()
+        
+        self.mainBodyText.insert(Tkinter.END, status)
+        self.mainBodyText.config(state=Tkinter.DISABLED)
+        
         self.entryVariable = Tkinter.StringVar()
-        self.entry = Tkinter.Entry(self, textvariable=self.entryVariable)
-        self.entry.grid(column=0, row=0, sticky='EW')
-        self.entry.bind("<Return>", self.OnPressEnter)
-        #self.entryVariable.set(u"Enter text here.")
-
-        button = Tkinter.Button(self, text=u"Send",
-                                command=self.OnButtonClick)
-        button.grid(column=1, row=0)
-
-        self.labelVariable = Tkinter.StringVar()
-        label = Tkinter.Message(self, textvariable=self.labelVariable,
-                              anchor="w", fg="black", bg="grey")
-        label.grid(column=0, row=1, columnspan=2, sticky='EW')
-        self.labelVariable.set(status)
-
-        self.grid_columnconfigure(0, weight=1)
-        self.resizable(True, False)
-        self.update()
-        self.geometry(self.geometry())       
-        self.entry.focus_set()
-        self.entry.selection_range(0, Tkinter.END)
+        self.textInput = Tkinter.Entry(self, width=60, textvariable=self.entryVariable)
+        self.textInput.bind("<Return>", self.OnPressEnter)
+        self.textInput.pack()
+        
+#        self.grid()
+#
+#        self.entryVariable = Tkinter.StringVar()
+#        self.entry = Tkinter.Entry(self, textvariable=self.entryVariable)
+#        self.entry.grid(column=0, row=0, sticky='EW')
+#        self.entry.bind("<Return>", self.OnPressEnter)
+#        #self.entryVariable.set(u"Enter text here.")
+#
+#        button = Tkinter.Button(self, text=u"Send",
+#                                command=self.OnButtonClick)
+#        button.grid(column=1, row=0)
+#
+#        self.labelVariable = Tkinter.StringVar()
+#        label = Tkinter.Message(self, textvariable=self.labelVariable,
+#                                anchor="w", fg="black", bg="grey")
+#        label.grid(column=0, row=1, columnspan=2, sticky='EW')
+#        self.labelVariable.set(status)
+#
+#        self.grid_columnconfigure(0, weight=1)
+#        self.resizable(True, False)
+#        self.update()
+#        self.geometry(self.geometry())       
+#        self.entry.focus_set()
+#        self.entry.selection_range(0, Tkinter.END)
         
         #start_new_thread(, (self,))
         threading.Thread(target=self.getMessage).start()
@@ -134,9 +149,6 @@ class simpleapp_tk(Tkinter.Tk):
 #                        self.labelVariable.set(data['username']+": "+data['msg']+"\n"+self.labelVariable.get())
                 
 
-    def OnButtonClick(self):
-        self.sendMessage()
-
     def OnPressEnter(self, event):
         self.sendMessage()
         
@@ -146,11 +158,15 @@ class simpleapp_tk(Tkinter.Tk):
             self.sock.send(json.dumps({'username': self.user}))
         else:
             self.sock.send(json.dumps({'username': self.user, 'msg': self.entryVariable.get().rstrip('\n')}))
-            self.labelVariable.set("you: "+self.entryVariable.get()+"\n"+self.labelVariable.get())
+            #self.labelVariable.set("you: " + self.entryVariable.get() + "\n" + self.labelVariable.get())
+            self.mainBodyText.config(state=Tkinter.NORMAL)
+            self.mainBodyText.insert(Tkinter.END, "you: " + self.entryVariable.get() + "\n")
+            self.mainBodyText.yview(Tkinter.END)
+            self.mainBodyText.config(state=Tkinter.DISABLED)
             
         self.entryVariable.set("")
-        self.entry.focus_set()
-        self.entry.selection_range(0, Tkinter.END)
+        self.textInput.focus_set()
+        self.textInput.selection_range(0, Tkinter.END)
     
     def getMessage(self):
         while 1:
@@ -158,9 +174,13 @@ class simpleapp_tk(Tkinter.Tk):
                 data = self.sock.recv(4096)
                 if data:
                     data = json.loads(data)
-                    print data['username']
+                    #print data['username']
                     #if (data['username'] != self.user) or (self.user == "Guest"):
-                    self.labelVariable.set(data['username']+": "+data['msg']+"\n"+self.labelVariable.get())
+                    #self.labelVariable.set(data['username'] + ": " + data['msg'] + "\n" + self.labelVariable.get())
+                    self.mainBodyText.config(state=Tkinter.NORMAL)
+                    self.mainBodyText.insert(Tkinter.END, data['username'] + ": " + data['msg'] + "\n")
+                    self.mainBodyText.yview(Tkinter.END)
+                    self.mainBodyText.config(state=Tkinter.DISABLED)
             except:
                 continue
 
